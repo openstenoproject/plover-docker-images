05ba9058181aed32c7c32304c53541fee53d2973
Benoit Pierre <benoit.pierre@gmail.com>
2019-05-20 18:53:15 +0200

    tweak build script to improve caching
    Tag intermediate stages so they don't get removed by `clean.sh`.

--- build.sh
+++ build.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
+set -e
+
 repo='plover/packpack'
 image="$1"
 shift
 
 image_size()
 {
-  docker images --format='{{.Size}}' "$repo:$image"
+  docker images --format='{{.Size}}' "$repo:$image" || true
 }
 
 previous_size="$(image_size)"
-docker build -t "$repo:$image" "$image" "$@"
+docker build -t "$repo:$image" "$image" "$@" &&
+# Tag intermediate stages so `./clean.sh` does not get rid of them.
+sed -n 's/^FROM [^ ]\+\(:[^ ]\+\)\? AS \([^ ]\+\)$/\2/p' "$image/Dockerfile" |
+tac | while read stage
+do
+  echo -n "Tagging stage $stage: "
+  docker build --quiet --target="$stage" -t "$repo:$image-$stage" "$image" "$@"
+done
 code=$?
-if [ $code -eq 0 -a -n "$previous_size" ]
+if [ -n "$previous_size" ]
 then
   echo "Image size: $previous_size -> $(image_size)"
 fi
